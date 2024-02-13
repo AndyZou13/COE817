86b5b43add779201954660a8fd55d2c5660a0154
Andy Zou <andyzou1302@gmail.com>
2024-02-13 11:16:01 -0500

    update lab 2

--- COE817_lab2/src/p1/client.java
+++ COE817_lab2/src/p1/client.java
@@ -5,24 +5,64 @@ import java.net.*;
 import java.security.*;
 import java.util.*;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
 public class client {
 
 	public static void main(String[] args) {
 		String id = "Alice";
-		int port = 50000;
+		int port = 50001;
 		String host = "localhost";
-		
+		String keyAB = "LAB2KEYAB";
+		Cipher ciph;
+		SecretKey sec;
+		byte[] encryptedIn = null;
+		byte[] encryptedOut = null;
+		byte[] decryptedIn = null;
 		try {
 			Socket client = new Socket (host, port);
 			System.out.println("Connected: " + client.getRemoteSocketAddress()); 
+			
 			SecureRandom n = new SecureRandom();
 			byte[] nBytes = new byte[5];
 			n.nextBytes(nBytes);
-			String message = id + Base64.getEncoder().encodeToString(nBytes);
-			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-			out.println(message);
-		} catch (Exception e) {
+			String message = id + "|" + Base64.getEncoder().encodeToString(nBytes);
+			DataOutputStream out = new DataOutputStream(client.getOutputStream());
+			out.writeUTF(message);
 			
+			
+			DataInputStream in = new DataInputStream(client.getInputStream());
+			int len = in.readInt();
+			if (len > 0) {
+				encryptedIn = new byte[len];
+			}
+			in.read(encryptedIn, 0, len);
+			System.out.println("Recieved 2 encrypted: " + encryptedIn.toString());
+			
+			SecretKeyFactory fact = SecretKeyFactory.getInstance("DES");
+			byte[] b = keyAB.getBytes();
+			sec = fact.generateSecret(new DESKeySpec(b));
+			ciph = Cipher.getInstance("DES/ECB/PKCS5Padding");
+			ciph.init(Cipher.DECRYPT_MODE, sec);
+			
+			decryptedIn = ciph.doFinal(encryptedIn);
+			System.out.println("Recieved 2 decrypted: " + new String(decryptedIn));
+			String decryp = new String(decryptedIn);
+			String[] ar = decryp.split("\\|");
+			String messageOut = id + "|" + ar[0];
+			
+			ciph.init(Cipher.ENCRYPT_MODE, sec);
+			encryptedOut = ciph.doFinal(messageOut.getBytes());
+			out.writeInt(encryptedOut.length);
+			out.write(encryptedOut);
+			
+		} catch (SocketException e) {
+			System.out.println("Socket timed out.");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
--- COE817_lab2/src/p1/server.java
+++ COE817_lab2/src/p1/server.java
@@ -2,39 +2,67 @@ package p1;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.*;
 
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
 
 public class server {	
 	
 	public static void main(String[] args) {
 		String id = "Bob";
-		int port = 50000;
+		int port = 50001;
 		ServerSocket s;
-		String keyAB = "lab2";
+		String keyAB = "LAB2KEYAB";
+		Cipher ciph;
+		SecretKey sec;
+		byte[] encryptedIn = null;
+		byte[] encryptedOut = null;
+		byte[] decryptedIn = null;
 		try {
 			s = new ServerSocket(port);
 			Socket server = s.accept();
 			System.out.println("Connected: " + server.getRemoteSocketAddress());
 			
-			BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-			String messageIn = in.readLine();
-			String nonceA = messageIn.substring(messageIn.length()-7, messageIn.length());
-			String clientID = messageIn.substring(0, messageIn.length()-7);
+			DataInputStream in = new DataInputStream(server.getInputStream());
+			String message = in.readUTF();
+			String[] ar = message.split("\\|");
+			System.out.println("Recieved 1: " + message);
 			
 			SecureRandom n = new SecureRandom();
 			byte[] nBytes = new byte[5];
 			n.nextBytes(nBytes);
-			String nonceB = id + Base64.getEncoder().encodeToString(nBytes);
-			byte[] b = keyAB.getBytes();
-			SecretKeyFactory fact = SecretKeyFactory.getInstance("DES");
+			String nonceB = Base64.getEncoder().encodeToString(nBytes);
 			
-			String messageOut
-		} catch (Exception e) {
-			System.out.println("Error");
-		}
+			SecretKeyFactory fact = SecretKeyFactory.getInstance("DES");
+			byte[] b = keyAB.getBytes();
+			sec = fact.generateSecret(new DESKeySpec(b));
+			ciph = Cipher.getInstance("DES/ECB/PKCS5Padding");
+			ciph.init(Cipher.ENCRYPT_MODE, sec);
+			
+			String messageOut = nonceB + "|" + id + "|" + ar[1];
+			encryptedOut = ciph.doFinal(messageOut.getBytes());
+			DataOutputStream out = new DataOutputStream(server.getOutputStream());
+			out.writeInt(encryptedOut.length);
+			out.write(encryptedOut);
+			
+			int len = in.readInt();
+			if (len > 0) {
+				encryptedIn = new byte[len];
+			}
+			in.read(encryptedIn, 0, len);
+			System.out.println("Recieved 3 encrypted: " + encryptedIn.toString());
+			
+			ciph.init(Cipher.DECRYPT_MODE, sec);
+			decryptedIn = ciph.doFinal(encryptedIn);
+			System.out.println("Recieved 3 decrypted: " + new String(decryptedIn));
+			} catch (SocketException e) {
+				System.out.println("Socket timed out.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 
 }
